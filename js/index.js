af11e55cda84b694f06373bbf8e0ed2e2289a4d9
carlosguerra1272 <carlosguerra@gmail.com>
2022-08-26 18:52:58 -0500

    Correct Values

--- js/index.js
+++ js/index.js
@@ -12,13 +12,13 @@ const Palomita8 = document.querySelector("#Palomita8SVG");
 
 const FinalFunctionEjecution = () => {
     framesParticle(Palomita1, 30, 75, 9, 10, 45); 
-    framesParticle(Palomita2, 25, 75, 9, 11.625, 45); 
+    framesParticle(Palomita2, 27, 75, 9, 11.625, 45); 
     framesParticle(Palomita3, 20, 75, 9, 13.25, 45); //
-    framesParticle(Palomita4, 25, 75, 9, 14.875, 45); 
+    framesParticle(Palomita4, 29, 75, 9, 14.875, 45); // un poco mas fuerte, menos de 30
     framesParticle(Palomita5, 30, 75, 9, 16.5, 45); 
     framesParticle(Palomita6, 25, 75, 9, 18.125, 45); 
     framesParticle(Palomita7, 33, 75, 9, 19.75, 45); 
-    framesParticle(Palomita8, 20, 75, 9, 21.375, 45); 
+    framesParticle(Palomita8, 38, 75, 9, 21.375, 45);  // un poco mas fuerte
 }
 
  setInterval(FinalFunctionEjecution, 3000)
